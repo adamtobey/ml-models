2c9988f14b49087380f104274df950df1a3aaaf8
Adam Tobey <adamtobey@gmail.com>
2017-12-03 14:05:27 +0000

    Regularized linear regression

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -10,14 +10,32 @@ class ScalarBasisFunctions(object):
     def Polynomial(degree):
         return lambda x: np.concatenate([(x**d).reshape(1, -1) for d in range(degree + 1)]).T
 
+class Regularizers(object):
+
+    @staticmethod
+    def NoReg():
+        return lambda *x: x
+
+    @staticmethod
+    def L2(l2_cost):
+        rl2 = l2_cost ** 0.5
+        def transform(inputs, targets):
+            n_weights = inputs.shape[1]
+            t_in = np.concatenate([inputs, np.diag([rl2 for _ in range(n_weights)])])
+            t_out = np.concatenate([targets, [0 for _ in range(n_weights)]])
+            return t_in, t_out
+        return transform
+
 class LinearRegression(object):
-    def __init__(self, basis_function=lambda x: x):
+    def __init__(self, basis_function=lambda x: x, regularizer=Regularizers.NoReg()):
         self.weights = None
-        self.basis_function = basis_function
+        self.make_basis_matrix = basis_function
+        self.regularize = regularizer
 
     def fit(self, inputs, targets):
-        self.weights = np.linalg.lstsq(self.basis_function(inputs), targets)[0]
+        inputs, targets = self.regularize(self.make_basis_matrix(inputs), targets)
+        self.weights = np.linalg.lstsq(inputs, targets)[0]
 
     def predict(self, inputs):
         assert self.weights is not None, "Model must be trained before predicting"
-        return self.basis_function(inputs).dot(self.weights)
+        return self.make_basis_matrix(inputs).dot(self.weights)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from models.linear_regression import LinearRegression, ScalarBasisFunctions
+from models.linear_regression import LinearRegression, ScalarBasisFunctions, Regularizers
 import numpy as np
 
-regressor = LinearRegression(basis_function=ScalarBasisFunctions.Polynomial(2))
+regressor = LinearRegression(basis_function=ScalarBasisFunctions.Polynomial(2),
+    regularizer=Regularizers.L2(5))
 
 x_train = np.arange(4)
 y_train = x_train ** 2 + x_train + 3
@@ -11,5 +12,6 @@ y_test = x_test ** 2 + x_test + 3
 
 regressor.fit(x_train, y_train)
 
+print("Model weights: ", regressor.weights)
 print("Model fit: ", regressor.predict(x_test))
 print("Y test: ", y_test)
