9a05e6c5be057431fc65cbfd99c3b425ea90daa5
Adam Tobey <adamtobey@gmail.com>
2017-12-20 20:16:10 -0500

    Clean up some garbage

--- server.py
+++
@@ -1,8 +0,0 @@
-from bokeh.plotting import curdoc
-from plots.logistic_regression import InteractiveLogisticRegression
-import numpy as np
-
-data = [dict(x=[1], y=[1]), dict(x=[3], y=[3])]
-plot = InteractiveLogisticRegression(data)
-
-plot.render(curdoc())
--- test.py
+++
@@ -1,3 +0,0 @@
-from bokeh.plotting import figure
-p = figure()
-print(p.js_on_event)
