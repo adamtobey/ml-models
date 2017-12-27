4e7544809f57b9988c48e697f49bf3e966fc3d69
Adam Tobey <adamtobey@gmail.com>
2017-12-26 22:28:03 -0500

    wip

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -10,7 +10,35 @@ class LogisticRegressionCost(Function):
     def eval(self, X, t, w):
         affine = X.dot(w)
         z = 2 * t - 1
+<<<<<<< Updated upstream
         return -np.sum(np.log(self.sigmoid(z * affine)), axis=0)
+=======
+        return np.sum(np.log(1 + z * affine), axis=0)
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, X, t, w):
+        z = 2 * t - 1
+        sn = self.sigmoid(z * X.dot(w))
+        return -np.sum(((1 - sn) * z).reshape(-1, 1) * X, axis=0)
+
+    def hessian(self, X, t, w):
+        def single_hessian(x, t, w):
+            z = 2 * t - 1
+            a = z * w.dot(x)
+            B = np.exp(a) + 1
+            b = np.log(B)
+            A = np.exp(a - 2 * b)
+            diag = np.ones(x.shape) * z * (A + B**-2)
+            return np.outer(x, x) * A + np.diag(diag)
+        return sum(single_hessian(x, t, w) for x, t in zip(X, t))
+
+class RefLRC(Function):
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+>>>>>>> Stashed changes
 
     def gradient(self, X, t, w):
         z = 2 * t - 1
@@ -28,7 +56,11 @@ class LogisticRegressionCost(Function):
 
 class LogisticRegression(object):
 
+<<<<<<< Updated upstream
     def __init__(self, basis_function=BasisFunctions.Affine(), optimizer=NewtonsMethod()):
+=======
+    def __init__(self, basis_function=BasisFunctions.Affine(), optimizer=NewtonsMethod(5e-3)):
+>>>>>>> Stashed changes
         self.optimizer = optimizer
         self.weights = None
         self.weights_init = lambda d: np.random.rand(d) * 0.1
---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy as np
+from calculus import gradient
+from models.logistic_regression import LogisticRegressionCost, RefLRC
+
+x = np.arange(4).reshape((2,2))
+w = y = np.arange(2)
+
+r = RefLRC()
+t = LogisticRegressionCost()
+
+anal =
+
+print("Ref gradient: ", r.gradient(x, y, w))
+print("Test gradient: ", t.gradient(x, y, w))
+print("Ref hessian: ", r.hessian(x, y, w))
+print("Test hessian: ", t.hessian(x, y, w))
