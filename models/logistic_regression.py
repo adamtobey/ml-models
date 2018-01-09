b039b11720958dfb29a5742304cefd249edcf958
Adam Tobey <adamtobey@gmail.com>
2018-01-09 15:49:57 -0500

    Cleanup testing with Newtons method - keeping logistic regression cost plot

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,6 +1,5 @@
 import numpy as np
-from calculus import gradient
-from optimizers import NewtonsMethod, Function, GradientDescentOptimizer
+from optimizers import Function, GradientDescentOptimizer
 from linear_basis_functions import BasisFunctions
 
 class LogisticRegressionCost(Function):
@@ -23,6 +22,8 @@ class LogisticRegressionCost(Function):
         sn = self.sigmoid(z * X.dot(w))
         return -np.sum(((1 - sn) * z).reshape(-1, 1) * X, axis=0)
 
+    # for Newton's method -- have to be careful about this since
+    # the cost function is nearly linear at the extrema...
     def hessian(self, X, t, w):
         def single_hessian(x, t, w):
             z = 2 * t - 1
@@ -40,60 +41,14 @@ class LogisticRegressionCost(Function):
                 return np.outer(x, x) * A / ((1 + A)**2 + epsilon) + np.diag(diag)
         return sum(single_hessian(x, t, w) for x, t in zip(X, t))
 
-class RefLRC(Function):
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def eval(self, X, t, w):
-        affine = X.dot(w)
-        z = 2 * t - 1
-        return -np.sum(np.log(self.sigmoid(z * affine)), axis=0)
-
-    def gradient(self, X, t, w):
-        z = 2 * t - 1
-        sn = self.sigmoid(z * X.dot(w))
-        return -np.sum(((1 - sn) * z).reshape(-1, 1) * X, axis=0)
-
-    def hessian(self, X, t, w):
-        epsilon = 1e-6 # avoid dividing by zero
-        def single_hessian(x, t, w):
-            z = 2 * t - 1
-            a = np.exp(-z * w.dot(x)) # TODO sometimes explodes
-            diag = np.ones(x.shape) * z * (a**2 + a + epsilon) / ((1 + a)**2 + epsilon)
-            return np.outer(x, x) * a / ((1 + a)**2 + epsilon) + np.diag(diag)
-        return sum(single_hessian(x, t, w) for x, t in zip(X, t))
-
-class Test(Function):
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def eval(self, X, t, w):
-        affine = X.dot(w)
-        z = 2 * t - 1
-        return -np.sum(np.log(self.sigmoid(z * affine)), axis=0)
-
-    def gradient(self, X, t, w):
-        z = 2 * t - 1
-        sn = self.sigmoid(z * X.dot(w))
-        return -np.sum(((1 - sn) * z).reshape(-1, 1) * X, axis=0)
-
-    def hessian(self, X, t, w):
-        def single_hessian(x, t, w):
-            return gradient(lambda x: self.gradient(x, t, w), x)
-        return sum(single_hessian(x, t, w) for x, t in zip(X, t))
-
 class LogisticRegression(object):
 
-    def __init__(self, basis_function=BasisFunctions.Affine(), optimizer=NewtonsMethod(1e-1)):
+    def __init__(self, basis_function=BasisFunctions.Affine(), optimizer=GradientDescentOptimizer(learning_rate=1e-1, num_epochs=1000)):
         self.optimizer = optimizer
-        # self.optimizer = GradientDescentOptimizer(0.1, 1000)
         self.weights = None
         self.weights_init = lambda d: np.random.rand(d) * 0.1
         self.basis_function = basis_function
         self.cost = LogisticRegressionCost()
-        # self.cost = RefLRC()
 
     def fit(self, X, y, num_epochs):
         X = self.basis_function(X)
